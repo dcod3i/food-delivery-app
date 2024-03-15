88c806def61f92c9682247951c5a7282c03ce653
dcod3i <pvnck1995@gmail.com>
2024-03-15 12:49:06 +0530

    added initial client folder and README.md file

---
+++ client/constants/index.js
@@ -0,0 +1,128 @@
+
+export const categories = [
+    {
+        id: 1,
+        name: 'Pizza',
+        image: require('../assets/images/icons8-pizza-96.png'),
+    },
+    {
+        id: 2,
+        name: 'Burger',
+        image: require('../assets/images/icons8-cupcake-emoji-96.png'),
+    },
+    {
+        id: 3,
+        name: 'Italian',
+        image: require('../assets/images/icons8-hamburger-96.png'),
+    },
+    {
+        id: 4,
+        name: 'Chinese',
+        image: require('../assets/images/icons8-spaghetti-96.png'),
+    },
+    {
+        id: 5,
+        name: 'Noodles',
+        image: require('../assets/images/icons8-takeout-box-96.png'),
+    },
+    {
+        id: 36,
+        name: 'Sweets',
+        image: require('../assets/images/icons8-tropical-drink-96.png'),
+    },
+]
+
+export const featured = {
+    id: 1,
+    title: 'Hot and Spicy',
+    description: 'soft and tender fried chicken',
+    restaurants: [
+        {
+            id: 1,
+            name: 'Papa Johns',
+            image: require('../assets/images/n7yx-hero (1).jpg'),
+            description: 'Hot and Spicy pizzas',
+            lng: 38.2145602,
+            lat: -85.2145602,
+            address: '434 second street',
+            stars: 4,
+            reviews:'4.4k',
+            category: 'Fast Food',
+            dishes: [
+                {
+                    id: 1,
+                    name: 'Papa Johns',
+                    description: 'Hot and Spicy pizzas',
+                    price: 10,
+                    image: require('../assets/images/icons8-pizza-96.png')
+                   
+                },
+                {
+                    id: 2,
+                    name: 'Papa Johns',
+                    description: 'Hot and Spicy pizzas',
+                    price: 20,
+                    image: require('../assets/images/icons8-cupcake-emoji-96.png')
+                }
+            ]
+        },
+        {
+            id: 2,
+            name: 'Grand Maharaja',
+            image: require('../assets/images/n7yx-hero (1).jpg'),
+            description: 'Hot and Spicy pizzas',
+            lng: 38.2145602,
+            lat: -85.2145602,
+            address: '434 second street',
+            stars: 4,
+            reviews:'4.4k',
+            category: 'Fast Food',
+            dishes: [
+                {
+                    id: 1,
+                    name: 'Papa Johns',
+                    description: 'Hot and Spicy pizzas',
+                    price: 10,
+                    image: require('../assets/images/icons8-pizza-96.png')
+                   
+                },
+                {
+                    id: 2,
+                    name: 'Papa Johns',
+                    description: 'Hot and Spicy pizzas',
+                    price: 20,
+                    image: require('../assets/images/icons8-cupcake-emoji-96.png')
+                }
+            ]
+        },
+        {
+            id: 3,
+            name: 'Taj Hotel',
+            image: require('../assets/images/n7yx-hero (1).jpg'),
+            description: 'Hot and Spicy pizzas',
+            lng: 38.2145602,
+            lat: -85.2145602,
+            address: '434 second street',
+            stars: 4,
+            reviews:'4.4k',
+            category: 'Fast Food',
+            dishes: [
+                {
+                    id: 1,
+                    name: 'Papa Johns',
+                    description: 'Hot and Spicy pizzas',
+                    price: 10,
+                    image: require('../assets/images/icons8-pizza-96.png')
+                   
+                },
+                {
+                    id: 2,
+                    name: 'Papa Johns',
+                    description: 'Hot and Spicy pizzas',
+                    price: 20,
+                    image: require('../assets/images/icons8-cupcake-emoji-96.png')
+                }
+            ]
+        },
+    ]
+}
