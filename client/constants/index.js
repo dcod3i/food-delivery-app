c336d858a35029271dcfdfe93bd6531f36cb88ef
Pavan Kumar Shetty <pavankumarck.scs20@rvce.edu.in>
2024-03-18 13:23:41 +0530

    Add files via upload

--- client/constants/index.js
+++ client/constants/index.js
@@ -126,3 +126,39 @@ export const featured = {
         },
     ]
 }
+
+export const featured_ = {
+    id: 1,
+    title: 'Hot and Spicy',
+    description: 'soft and tender fried chicken',
+    restaurants: [
+        {
+            id: 1,
+            name: 'Papa Johns',
+            image: require('../assets/images/n7yx-hero (1).jpg'),
+            description: 'Hot and Spicy pizzas',
+            lng: 38.2145602,
+            lat: -85.2145602,
+            address: '434 second street',
+            stars: 4,
+            reviews: '4.4k',
+            category: 'Fast Food',
+            dishes: [
+                {
+                    id: 1,
+                    name: 'Papa Johns',
+                    description: 'Hot and Spicy pizzas',
+                    price: 10,
+                    image: require('../assets/images/icons8-pizza-96.png')
+                },
+                {
+                    id: 2,
+                    name: 'Papa Johns',
+                    description: 'Hot and Spicy pizzas',
+                    price: 20,
+                    image: require('../assets/images/icons8-cupcake-emoji-96.png')
+                }
+            ]
+        }
+    ]
+}
